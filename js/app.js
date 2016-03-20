0b11c544c35d6442720fc4005cb205eb40c9b2b3
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-19 17:04:38 -0700

    first update of tic tac toe

--- js/app.js
+++ js/app.js
@@ -3,4 +3,23 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+  var count = 0;
+
+  $(".box").on("click", function playerTurn(event){
+    if(! $(this).text()){
+      if(count % 2 === 0){
+        $(this).text("X");
+        count++;
+      }else if(count % 2 !== 0){
+        $(this).text("O");
+        count++;
+      }
+    }
+  });
+
+
+  $(".btn").on("click", function restartGame(event){
+       $(".box").text("");
+  });
+
 });
