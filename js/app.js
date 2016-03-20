48679d02cfb792a3cc75da6d02b42d2d86f91048
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-20 12:03:26 -0700

    4th update

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
   var playerTwo = 'O';
   var count = 0;
   var tacBoxes = $(".box");
+  var count1 = 0;
+  var count2 = 0;
+  var countd = 0;
 
   $(".box").on("click", function playerTurn(event){
     if(! $(this).text()){
@@ -17,10 +20,11 @@ $(document).ready(function() {
         count++;
       }
       winner();
+      countsWinner();
     }
 });
   function winner(){
-    var gameWinner = " ";
+    var gameWinner;
     if( ($(tacBoxes.eq(0)).text()==="X" &&
          $(tacBoxes.eq(1)).text()==="X" &&
          $(tacBoxes.eq(2)).text()==="X" ) ||
@@ -45,7 +49,7 @@ $(document).ready(function() {
         ($(tacBoxes.eq(2)).text()==="X" &&
          $(tacBoxes.eq(5)).text()==="X" &&
          $(tacBoxes.eq(8)).text()==="X" )){
-          gameWinner = "Player One";
+          gameWinner = "playerOneWins";
    }else if( ($(tacBoxes.eq(0)).text()==="O" &&
               $(tacBoxes.eq(1)).text()==="O" &&
               $(tacBoxes.eq(2)).text()==="O" ) ||
@@ -70,26 +74,44 @@ $(document).ready(function() {
              ($(tacBoxes.eq(2)).text()==="O" &&
               $(tacBoxes.eq(5)).text()==="O" &&
               $(tacBoxes.eq(8)).text()==="O" )){
-            gameWinner = "Player Two";
-              }else {
-                if(($(tacBoxes.eq(0)).text()==="" ||
-                           $(tacBoxes.eq(1)).text()==="" ||
-                           $(tacBoxes.eq(2)).text()==="" ||
-                           $(tacBoxes.eq(3)).text()==="" ||
-                           $(tacBoxes.eq(4)).text()==="" ||
-                           $(tacBoxes.eq(5)).text()==="" ||
-                           $(tacBoxes.eq(6)).text()==="" ||
-                           $(tacBoxes.eq(7)).text()==="" ||
-                           $(tacBoxes.eq(8)).text()==="" )){
-                             gameWinner = '';
-                           }else gameWinner = ("Draw");
+            gameWinner = "playerTwoWins";
+          }else {
+          if(($(tacBoxes.eq(0)).text()==="" ||
+                    $(tacBoxes.eq(1)).text()==="" ||
+                    $(tacBoxes.eq(2)).text()==="" ||
+                    $(tacBoxes.eq(3)).text()==="" ||
+                    $(tacBoxes.eq(4)).text()==="" ||
+                    $(tacBoxes.eq(5)).text()==="" ||
+                    $(tacBoxes.eq(6)).text()==="" ||
+                    $(tacBoxes.eq(7)).text()==="" ||
+                    $(tacBoxes.eq(8)).text()==="" )){
+                             gameWinner = "";
+                  }else {
+                    gameWinner = "Draw";
+                  }
+                }
+                  return gameWinner;
           }
-          console.log (gameWinner);
-          return gameWinner;
-
-    }
-    
+          function countsWinner(){
+            var gameWinner = winner();
+          if(gameWinner === "playerOneWins"){
+            alert(gameWinner);
+            count1++;
+            console.log(count1);
+          }else if (gameWinner === "playerTwoWins") {
+            alert(gameWinner);
+            count2++;
+            console.log(count2);
+          }else if (gameWinner === "Draw") {
+            alert(gameWinner);
+            countd++;
+            console.log(countd);
+          }
+        }
        $(".btn").on("click", function restartGame(event){
             $(".box").text("");
        });
-  });
+       $("#btn2").on("click", function resetWholeGame(event){
+         window.location.reload();
+       });
+});
